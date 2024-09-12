ce070dd3ba5436057341dc7d5595d9ae44d823c5
Jin <rhkdghks21@naver.com>
2024-09-12 17:05:56 +0900

    [FEAT]: 인메모리 방식 시큐리티 구현 - 로그인화면 구성 예정 - 로그인을 하면 자동으로 접속되는 화면 구현 예정

---
+++ src/main/java/com/spring/moji/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package com.spring.moji.config;
+
+import static org.springframework.security.config.Customizer.*;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
+
+		http.authorizeHttpRequests(auth -> auth
+			.requestMatchers("/admin/**").hasRole("ADMIN")
+			.requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+			.anyRequest().permitAll())
+			.formLogin(withDefaults())
+			.logout(withDefaults());
+
+		http.logout(logout -> logout
+			.logoutUrl("/logout")
+			.logoutSuccessUrl("/")
+			.deleteCookies("JSESSIONID")
+			.invalidateHttpSession(true));
+		return http.build();
+
+	}
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.builder()
+			.username("user")
+			.password(passwordEncoder().encode("1004"))
+			.roles("USER")
+			.build();
+
+		UserDetails admin = User.builder()
+			.username("admin")
+			.password(passwordEncoder().encode("1007"))
+			.roles("USER","ADMIN")
+			.build();
+
+		return new InMemoryUserDetailsManager(user,admin);
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
