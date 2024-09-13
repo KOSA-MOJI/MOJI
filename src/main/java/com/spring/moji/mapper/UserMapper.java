786bc6a863c89fcce578a5fe7cb90b8cc53e71da
Jin <rhkdghks21@naver.com>
2024-09-13 16:59:34 +0900

    [FEAT]: UserMapper에 로그인 메서드 추가

---
+++ src/main/java/com/spring/moji/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.spring.moji.mapper;
+
+
+import com.spring.moji.entity.UserEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+  public UserEntity login(String email);
+}
