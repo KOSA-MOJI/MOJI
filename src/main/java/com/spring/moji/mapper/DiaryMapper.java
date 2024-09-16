09f1fa6db977d60bb324cfa5cb49ea9c7ffea405
cookooyas <cookooyas@gmail.com>
2024-09-16 21:55:39 +0900

    feature: 다이어리 매퍼의 파라미터를 위한 DTO 설계

--- src/main/java/com/spring/moji/mapper/DiaryMapper.java
+++ src/main/java/com/spring/moji/mapper/DiaryMapper.java
@@ -2,10 +2,11 @@ package com.spring.moji.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.spring.moji.dto.request.DiaryRequestDTO;
 import com.spring.moji.entity.Diary;
 
 @Mapper
 public interface DiaryMapper {
-	Diary findByDiaryId(Long diaryId);
 	Diary findByCoupleId(Long coupleId);
+	void updateCoverImage(DiaryRequestDTO diaryRequestDTO);
 }
