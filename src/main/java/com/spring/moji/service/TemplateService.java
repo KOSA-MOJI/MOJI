9bb899c23a75905d20e76f44623341a430f212e7
cookooyas <cookooyas@gmail.com>
2024-09-17 05:15:14 +0900

    feature: 템플릿 서비스 구현

---
+++ src/main/java/com/spring/moji/service/TemplateService.java
@@ -0,0 +1,9 @@
+package com.spring.moji.service;
+
+import java.util.List;
+
+import com.spring.moji.entity.Template;
+
+public interface TemplateService {
+	List<Template> findAll();
+}
---
+++ src/main/java/com/spring/moji/service/TemplateServiceImpl.java
@@ -0,0 +1,23 @@
+package com.spring.moji.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.moji.entity.Template;
+import com.spring.moji.mapper.TemplateMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class TemplateServiceImpl implements TemplateService {
+	private final TemplateMapper templateMapper;
+
+	@Override
+	public List<Template> findAll() {
+		return templateMapper.findAll();
+	}
+}
