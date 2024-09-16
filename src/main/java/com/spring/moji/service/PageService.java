52738c815091ba83d059810fadd588497cc155da
cookooyas <cookooyas@gmail.com>
2024-09-17 04:03:36 +0900

    fix : 페이지 서비스 인터페이스 접근 제한자 오류 수정

--- src/main/java/com/spring/moji/service/PageService.java
+++ src/main/java/com/spring/moji/service/PageService.java
@@ -6,6 +6,6 @@ import java.util.List;
 import com.spring.moji.entity.Page;
 
 public interface PageService {
-	public List<Page> fetchDiaryPages(Long diaryId, LocalDate startDate, LocalDate endDate);
-	public Page findRecentPage(Long diaryId);
+	List<Page> fetchDiaryPages(Long diaryId, LocalDate startDate, LocalDate endDate);
+	Page findRecentPage(Long diaryId);
 }
