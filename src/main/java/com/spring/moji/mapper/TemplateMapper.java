6c003613180bad37014dfc2132b1f147714c06cc
cookooyas <cookooyas@gmail.com>
2024-09-17 05:15:06 +0900

    feature: 템플릿 매퍼 구현

---
+++ src/main/java/com/spring/moji/mapper/TemplateMapper.java
@@ -0,0 +1,12 @@
+package com.spring.moji.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.spring.moji.entity.Template;
+
+@Mapper
+public interface TemplateMapper {
+	List<Template> findAll();
+}
