cdb597647364f463611afc9a5c730633d645bc93
cookooyas <cookooyas@gmail.com>
2024-09-21 21:21:59 +0900

    feature : 커뮤니티 REST API 중 스크랩 추가, 삭제 컨트롤러 API 구현

--- src/main/java/com/spring/moji/controller/CommunityRestController.java
+++ src/main/java/com/spring/moji/controller/CommunityRestController.java
@@ -1,5 +1,6 @@
 package com.spring.moji.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.spring.moji.dto.request.CommunityRequestDTO;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,11 +34,17 @@ public class CommunityRestController {
 		return communityService.getPublicPage(pageId);
 	}
 	@PostMapping("/scrap")
-	public boolean addScrap(@ModelAttribute CommunityScrapRequestDTO communityScrapRequestDTO) {
+	public boolean addScrap(@RequestParam String email, @RequestParam Long pageId) {
+		ArrayList<Long> pageIds = new ArrayList<>();
+		pageIds.add(pageId);
+		CommunityScrapRequestDTO communityScrapRequestDTO = CommunityScrapRequestDTO.builder().email(email).pageIds(pageIds).build();
 		return communityService.addScrap(communityScrapRequestDTO);
 	}
 	@DeleteMapping("/scrap")
-	public boolean removeScrap(@ModelAttribute CommunityScrapRequestDTO communityScrapRequestDTO) {
+	public boolean removeScrap(@RequestParam String email, @RequestParam Long pageId) {
+		ArrayList<Long> pageIds = new ArrayList<>();
+		pageIds.add(pageId);
+		CommunityScrapRequestDTO communityScrapRequestDTO = CommunityScrapRequestDTO.builder().email(email).pageIds(pageIds).build();
 		return communityService.removeScrap(communityScrapRequestDTO);
 	}
 }
