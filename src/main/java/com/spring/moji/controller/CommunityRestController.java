9662c50522368ffc42b9ccd9cfc76da2af4bd4fc
cookooyas <cookooyas@gmail.com>
2024-09-21 20:48:00 +0900

    feature : 커뮤니티 REST API 중 스크랩 추가, 삭제, 단일 공개 페이지 조회 컨트롤러 정의

--- src/main/java/com/spring/moji/controller/CommunityRestController.java
+++ src/main/java/com/spring/moji/controller/CommunityRestController.java
@@ -3,12 +3,16 @@ package com.spring.moji.controller;
 import java.util.List;
 
 import com.spring.moji.dto.request.CommunityRequestDTO;
+import com.spring.moji.dto.request.CommunityScrapRequestDTO;
 import com.spring.moji.dto.response.CommunityResponseDTO;
 import com.spring.moji.entity.Page;
 import com.spring.moji.service.CommunityServiceImpl;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +27,17 @@ public class CommunityRestController {
     public List<CommunityResponseDTO> getCommunityUnderList(@ModelAttribute CommunityRequestDTO communityRequestDTO) {
         return communityService.getCommunityUnderList(communityRequestDTO);
     }
+	@RequestMapping("/page/{pageId}")
+	public Page getCommunityUnderListPage(@PathVariable Long pageId){
+		communityService.getPublicPage(pageId);
+		return null;
+	}
+	@PostMapping("/scrap")
+	public boolean addScrap(@ModelAttribute CommunityScrapRequestDTO communityScrapRequestDTO) {
+		return communityService.addScrap(communityScrapRequestDTO);
+	}
+	@DeleteMapping("/scrap")
+	public void removeScrap(@ModelAttribute CommunityScrapRequestDTO communityScrapRequestDTO) {
+		return communityService.removeScrap(communityScrapRequestDTO);
+	}
 }
