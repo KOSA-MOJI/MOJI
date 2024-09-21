eb806b2ac8e0c0bd3f48464b70ccd0a204fad26a
cookooyas <cookooyas@gmail.com>
2024-09-21 20:39:43 +0900

    feature : 커뮤니티 REST API 를 위한 스크랩 엔티티, 매퍼, 쿼리 구현

---
+++ src/main/java/com/spring/moji/mapper/ScrapMapper.java
@@ -0,0 +1,13 @@
+package com.spring.moji.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.spring.moji.dto.request.CommunityScrapRequestDTO;
+import com.spring.moji.entity.Scrap;
+
+@Mapper
+public interface ScrapMapper {
+	List<Scrap> findAllByPageIds(CommunityScrapRequestDTO communityScrapRequestDTO);
+}
