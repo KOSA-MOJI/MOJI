453b4dde83a9b10cf5b2527342e08df06c98f013
cookooyas <cookooyas@gmail.com>
2024-09-24 06:12:55 +0900

    feature : 커뮤니티 서비스 지도 조회를 위한 API키 추가

--- src/main/java/com/spring/moji/controller/CommunityController.java
+++ src/main/java/com/spring/moji/controller/CommunityController.java
@@ -1,6 +1,8 @@
 package com.spring.moji.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/community")
 public class CommunityController {
-
+	@Value("${kakao.api-key}")
+	private String kakaoApiKey;
     //커뮤니티 컴포넌트 제대로 뜨는가
     @GetMapping()
     public String readCommunity(Model model) {
+		model.addAttribute("kakaoApiKey", kakaoApiKey);
         model.addAttribute("contentURL", "/WEB-INF/jsp/content/community/community-diary.jsp");
         return "community/community-diary-page";
     }
