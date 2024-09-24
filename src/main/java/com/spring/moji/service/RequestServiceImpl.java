f508fdbcd90ac3a5d7340bfd58a1161fe806a5d1
Kwanghwan Jin <rhkdghks21@naver.com>
2024-09-24 11:05:06 +0900

    [FIX]: 커플 신청 조건 수정

--- src/main/java/com/spring/moji/service/RequestServiceImpl.java
+++ src/main/java/com/spring/moji/service/RequestServiceImpl.java
@@ -1,13 +1,17 @@
 package com.spring.moji.service;
 
 
+import com.spring.moji.entity.Request;
 import com.spring.moji.entity.User;
 import com.spring.moji.mapper.RequestMapper;
 import com.spring.moji.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RequestServiceImpl implements RequestService {
@@ -18,22 +22,32 @@ public class RequestServiceImpl implements RequestService {
   @Override
   @Transactional
   public int requestCouple(String requestEmail, String receiverEmail) throws Exception {
-    int result;
-    User receiver = userMapper.findUserByEmail(receiverEmail);
+    log.info("요청을 보낸 이메일 {}", receiverEmail);
 
-    if (receiver != null) { // 해당 유저가 존재하면
-      if (receiver.getCoupleStatus().equals(1L)) {
-        return -1;
-      }
-      String existingRequestEmail = requestMapper.checkRequest(receiverEmail); // 요청이 있는 지 확인
-      if (existingRequestEmail == null) { // 요청이 없으면
-        return requestMapper.requestCouple(requestEmail, receiverEmail); // 요청 보내고 1을 반환
-      } else {
-        return 0;  // 요청이 있으면 0을 반환
-      }
-    } else {
-      return -1; // 해당 유저가 존재하지 않거나 커플이면 -1을 반환
+    User receiver = userMapper.findUserByEmail(receiverEmail);
+    log.info("요청을 보낸 이메일 {}", receiverEmail);
+
+    if (receiver == null) {  // 유저가 존재하지 않는 경우
+      return -1;
     }
+    log.info("요청을 보낸 이메일 {}", receiverEmail);
+
+    if (!receiver.getCoupleStatus().equals(0L)) {  // 유저가 커플인 경우
+      return 0;
+    }
+    log.info("요청을 보낸 이메일 {}", receiverEmail);
+
+    Request existingRequest = requestMapper.checkRequest(receiverEmail);
+    log.info("existingRequest {}", existingRequest.getRequestEmail());
+    log.info("existingRequest {}", existingRequest.getCreatedAt());
+ 
+    if (existingRequest == null) { // 이미 커플 신청을 받았을 경우
+      return requestMapper.requestCouple(requestEmail, receiverEmail);
+    }
+    log.info("현재 진행중인 리퀘스트 {}", existingRequest);
+    log.info("요청값이 null로 들어옴");
+
+    return 2;
   }
 
   @Override
@@ -49,10 +63,8 @@ public class RequestServiceImpl implements RequestService {
   }
 
   @Override
-  @Transactional
-  public User checkRequest(String email) throws Exception {
-    String requestEmail = requestMapper.checkRequest(email);
-    return userMapper.findUserByEmail(requestEmail);
+  public Request checkRequest(String receiverEmail) throws Exception {
+    return requestMapper.checkRequest(receiverEmail);
   }
 
   @Override
