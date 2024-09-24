01e378abd735efd707b0d1183d301495379e902b
Kwanghwan Jin <rhkdghks21@naver.com>
2024-09-24 21:42:09 +0900

    [FIX]: Communtity 컨트롤러 권한 설정
    - 모든 유저가 볼 수 있도록 권한 설정

--- src/main/java/com/spring/moji/controller/CommunityController.java
+++ src/main/java/com/spring/moji/controller/CommunityController.java
@@ -10,15 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/community")
+@RequestMapping("user/community")
 public class CommunityController {
-	@Value("${kakao.api-key}")
-	private String kakaoApiKey;
-    //커뮤니티 컴포넌트 제대로 뜨는가
-    @GetMapping()
-    public String readCommunity(Model model) {
-		model.addAttribute("kakaoApiKey", kakaoApiKey);
-        model.addAttribute("contentURL", "/WEB-INF/jsp/content/community/community-diary.jsp");
-        return "community/community-diary-page";
-    }
+
+  @Value("${kakao.api-key}")
+  private String kakaoApiKey;
+
+  //커뮤니티 컴포넌트 제대로 뜨는가
+  @GetMapping()
+  public String readCommunity(Model model) {
+    model.addAttribute("kakaoApiKey", kakaoApiKey);
+    model.addAttribute("contentURL", "/WEB-INF/jsp/content/community/community-diary.jsp");
+    return "community/community-diary-page";
+  }
 }
