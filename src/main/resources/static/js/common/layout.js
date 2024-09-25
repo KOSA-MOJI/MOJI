1890923850caadbe96571ac9fdca877951f37bc4
cookooyas <cookooyas@gmail.com>
2024-09-25 10:23:13 +0900

    comment : 불필요한 레이아웃 js코드 주석화

--- src/main/resources/static/js/common/layout.js
+++ src/main/resources/static/js/common/layout.js
@@ -1,9 +1,9 @@
 // 로그아웃 버튼 클릭 시 동작
-document.getElementById('logoutBtn').addEventListener('click', function() {
-    document.getElementById('Btn').style.display = 'none';  // 드롭다운 버튼 숨기기
-    document.getElementById('userInfo').style.display = 'none';  // 프로필과 이름 숨기기
-    document.getElementById('authOptions').style.display = 'flex';  // 회원가입/로그인 버튼 표시
-});
+// document.getElementById('logoutBtn').addEventListener('click', function() {
+//     document.getElementById('Btn').style.display = 'none';  // 드롭다운 버튼 숨기기
+//     document.getElementById('userInfo').style.display = 'none';  // 프로필과 이름 숨기기
+//     document.getElementById('authOptions').style.display = 'flex';  // 회원가입/로그인 버튼 표시
+// });
 
 document.addEventListener("DOMContentLoaded", function() {
     const iconLinks = document.querySelectorAll('.sidebar nav ul li a');
